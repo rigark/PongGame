5d5c1b68a91fcfc8492d0e8e65966a91fe647c00
rigark <74835326+rigark@users.noreply.github.com>
2022-06-14 09:18:19 +0800

    Add files via upload

---
+++ Window.java
@@ -0,0 +1,82 @@
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.Color;
+
+
+public class Window extends JFrame implements Runnable {
+
+   public Graphics2D g2;
+   public KL keyListener = new KL();
+   public Rect playerOne, ai, ballRect;
+   public PlayerController playerController;
+   public AiController aiController;
+   public Ball ball;
+   public Text leftScoreText, rightScoreText;
+   public boolean isRunning = true;
+
+
+    public Window() {
+        this.setSize(Constants.SCREEN_WIDTH, (Constants.SCREEN_HEIGHT));
+        this.setTitle(Constants.SCREEN_TITLE);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(keyListener);
+        Constants.TOOLBAR_HEIGHT = this.getInsets().top;
+        Constants.INSETS_BOTTOM = this.getInsets().bottom;
+
+        leftScoreText = new Text(0, new Font("Times New Roman",Font.PLAIN,Constants.TEXT_SIZE),Constants.TEXT_X_POS,Constants.TEXT_V_POS);
+        rightScoreText = new Text(0,
+                new Font("Times New Roman",Font.PLAIN,Constants.TEXT_SIZE),
+                Constants.SCREEN_WIDTH - Constants.TEXT_X_POS - 16,Constants.TEXT_V_POS);
+
+        g2 = (Graphics2D)this.getGraphics();
+
+        playerOne = new Rect(Constants.HZ_PADDING,40,Constants.PADDLE_WIDTH,Constants.PADDLE_HEIGHT,Constants.PADDLE_COLOR);
+        playerController = new PlayerController(playerOne,keyListener);
+
+        ai = new Rect(Constants.SCREEN_WIDTH-Constants.PADDLE_WIDTH-Constants.HZ_PADDING,40,Constants.PADDLE_WIDTH,Constants.PADDLE_HEIGHT,Constants.PADDLE_COLOR);
+        ballRect = new Rect(Constants.SCREEN_WIDTH / 2.0,Constants.SCREEN_HEIGHT / 2.0,Constants.BALL_WIDTH,Constants.BALL_WIDTH,Constants.PADDLE_COLOR);
+        ball = new Ball(ballRect,playerOne,ai, leftScoreText,rightScoreText);
+        aiController= new AiController(new PlayerController(ai),ballRect);
+
+    }
+    public void update(double dt){
+        Image dbImage = createImage(getWidth(),getHeight());
+        Graphics dbg = dbImage.getGraphics();
+        this.draw(dbg);
+        g2.drawImage(dbImage, 0, 0, this);
+
+       playerController.update(dt);
+       aiController.update(dt);
+       ball.update(dt);
+    }
+    public void draw(Graphics g){
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.GREEN);
+        g2.fillRect(0,0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+
+        leftScoreText.draw(g2);
+        rightScoreText.draw(g2);
+        playerOne.draw(g2);
+        ai.draw(g2);
+        ballRect.draw(g2);
+    }
+
+public void stop(){
+        isRunning = false;
+}
+    public void run() {
+        double lastFrameTime =0.0;
+        while (isRunning) {
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
+
+            update(deltaTime);
+
+        }
+        this.dispose();
+        return;
+    }
+}
